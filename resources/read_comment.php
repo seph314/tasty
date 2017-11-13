7f1bfbc55257e495a2fd8ebea1665ded0d2e3956
Anders <andpet52@kth.se>
2017-11-13 17:12:42 +0100

    fungerar att läsa och skriva kommentarer

--- resources/read_comment.php
+++ resources/read_comment.php
@@ -1,18 +1,20 @@
 <?php
 
 $mydish = $_SESSION['dish'];
-$comment = mysqli_query($db,"select * from comment where dish = 'meatballs'");
+$comm = mysqli_query($db,"select * from comment where dish = 'meatballs'");
 
-while ($row = mysqli_fetch_array($comment)) {
+    while($row=mysqli_fetch_array($comm)) {
     $name = $row['name'];
     $comment = $row['comment'];
     $time = $row['post_time'];
+
 ?>
 
 <div class="darker">
         <h3>Comment from <strong><?php echo $name;?></strong><?php echo $time;?></h3>
 <p><?php echo $comment;?></p>
 </div>
+
 
 <?php
 }
